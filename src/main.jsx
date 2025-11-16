5b60ff3749b3e164b7cfda21f9a1a0684555c245
sadvi134ash2004 <sadvibayyavarapu@gmail.com>
2025-11-16 13:46:56 +0530

    refactor routing structure to use HashRouter instead of RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./state/AuthContext";
 
-const router = createBrowserRouter([{ path: "*", element: <App /> }]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <HashRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
   </React.StrictMode>
 );
